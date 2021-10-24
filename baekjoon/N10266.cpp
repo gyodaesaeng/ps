f3d29dd2e664bfe3caa30348363d4c4a6c2fdba3
gyodaesaeng <iadev20@gmail.com>
2021-10-24 20:37:21 +0900

    백준 10266

--- baekjoon/N10266.cpp
+++ baekjoon/N10266.cpp
@@ -1,24 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, in, pi[720000];
+int n, in, pi[360000];
 bool a[720000], b[360000];
 bool kmp()
 {
 	memset(pi, 0, sizeof(pi));
 	int j = 0;
-	for (int i = 1; i < 720000; i++)
+	for (int i = 1; i < 360000; i++)
 	{
-		while (j && a[i] != b[j])
+		while (j && b[i] != b[j])
 			j = pi[j - 1];
-		if (a[i] == b[j])
-		{
+		if (b[i] == b[j])
 			pi[i] = ++j;
-			if (j == 360000)
-				return true;
-		}
+	}
+	j = 0;
+	for (int i = 0; i < 720000; i++)
+	{
+		while (j > 0 && a[i] != b[j])
+			j = pi[j - 1];
+		if (a[i] == b[j] && ++j == 360000)
+			return true;
 	}
 	return false;
 }
+
 int main()
 {
 	scanf("%d", &n);
